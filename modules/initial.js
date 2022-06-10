e83e122cf49d4bf2a4fce59510de180b8826f66f
Artem Pushnev <artem.pushnev@gmail.com>
2022-06-10 10:43:22 +0300

    fix padding

--- modules/initial.js
+++ modules/initial.js
@@ -3,12 +3,6 @@ import { Drawer } from './drawer.js';
 export const initial = {
   init() {
     const form = document.querySelector('.section_tickets__calculation');
-    const menuBar = document.querySelector('.section-welcome__menu-bar');
-    menuBar.classList.remove('open');
-    const menuBtn = document.querySelector('.header__menu-burger');
-    menuBtn.classList.remove('open');
-    const welcomeText = document.querySelector('.section-welcome__text');
-    welcomeText.classList.remove('open');
 
     form.onsubmit = function () {
       if (Drawer.isValid()) {
